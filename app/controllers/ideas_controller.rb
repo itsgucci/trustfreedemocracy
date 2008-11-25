e0e58b6f1df656f081d70a38d5dbfecf63009b86
Eric Weinert <drake@hungrychild.org>
2008-11-25 01:49:05 -1000

    redid all help in nice folding javascript this should be generalized. look how much redundant code there was. too lazy to figure it out right now

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,9 +3,9 @@ class IdeasController < ApplicationController
   def index
     
     if current_district
-      @list = current_district.articles.if_certified( certification_filter? ).drafts#.tag_filter(sieve.tags)
+      @list = current_district.articles.if_certified( certification_filter? ).drafts.paginate(:per_page => 7, :page => params[:page])#.tag_filter(sieve.tags)
     else
-      @list = current_community.articles.if_certified( certification_filter? ).drafts#.tag_filter(sieve.tags) 
+      @list = current_community.articles.if_certified( certification_filter? ).drafts.paginate(:per_page => 7, :page => params[:page])#.tag_filter(sieve.tags) 
     end
     
     respond_to do |format|
