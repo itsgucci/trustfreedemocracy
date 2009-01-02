1186816e4d417e117197bfc64221eaa5df3623e6
Eric Weinert <drake@hungrychild.org>
2009-01-01 19:37:08 -1000

    better still

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -7,18 +7,18 @@ class FacebookController < ApplicationController
   def index    
     #debugger
     unless logged_in?
-      if user= User.find_by_facebook_id(fbsession.session_user_id)
+      if user= User.find_by_facebook_id(facebook_user)
         self.current_user = user
       end
     end
     unless logged_in? 
       new_user = User.new
-      new_user.facebook_id = facebook_user.id
+      new_user.facebook_id = facebook_user
       # todo: purge this nasty
       new_user.save_without_validation
       self.current_user = new_user.reload
     end
-    flash[:notice] = "<p>Aloha, <fb:name uid='loggedinuser'></fb:name></p>"
+    flash[:notice] = "<p>Aloha, <fb:name uid='loggedinuser' useyou='false'></fb:name></p>"
     redirect_to '/'
   end
   
